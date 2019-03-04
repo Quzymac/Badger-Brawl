983c72275d574c678075d3fbfa26b4d5508791bb
Quzymac <33623813+Quzymac@users.noreply.github.com>
2019-03-04 12:13:03 +0100

    Platform/player whitebox, simple movement for testing

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+
+    [SerializeField] float moveSpeed = 5f;
+   
+
+    private void FixedUpdate()
+    {
+         //calculate movement velocity as a 3D vector
+        float xMov = Input.GetAxisRaw("Horizontal");
+
+        Vector3 moveHorizontal = transform.right * xMov;
+
+        //apply movement
+        transform.Translate(moveHorizontal * Time.deltaTime * moveSpeed);
+    }
+}
