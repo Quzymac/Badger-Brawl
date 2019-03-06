07baa82f6565aaebb280d0b9563759281b0a1dc8
TobenTheGreat <33621588+TobenTheGreat@users.noreply.github.com>
2019-03-06 15:53:48 +0100

    Lager och eget script för colllision

---
+++ Assets/Scripts/JumpScript.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class JumpScript : MonoBehaviour
+{
+    [SerializeField]
+    Rigidbody playerbody;
+    Vector3 platformPosition;
+    int colliderValue = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerbody = GetComponent<Rigidbody>();
+        Physics.IgnoreLayerCollision(10, 10);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.collider.gameObject.layer == LayerMask.NameToLayer("Platform"))
+        {
+            Debug.Log("Hey platorm");
+            Physics.IgnoreLayerCollision(9, 10);
+        }
+    }
+}
