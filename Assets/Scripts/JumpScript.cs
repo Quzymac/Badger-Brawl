11f689a7c658599224830e304ad95a05ba379e37
TobenTheGreat <33621588+TobenTheGreat@users.noreply.github.com>
2019-03-07 13:51:05 +0100

    Half fungerande hoppa igenom script

--- Assets/Scripts/JumpScript.cs
+++ Assets/Scripts/JumpScript.cs
@@ -11,47 +11,50 @@ public class JumpScript : MonoBehaviour
     [SerializeField]
     GameObject collidedPlatform;
 
-    //Vector3 platformPosition;
-    int colliderValue = 0;
-
-    // Start is called before the first frame update
     void Start()
     {
         playerbody = GetComponent<Rigidbody>();
         player = GetComponent<Collider>();
     }
 
-    // Update is called once per frame
     void Update()
     {
         if (Input.GetKeyDown(KeyCode.UpArrow))
         {
+
             gameObject.layer = 12;
-            Debug.Log(collidedPlatform);
-            //Physics.IgnoreLayerCollision(9, 10);          
-            Physics.IgnoreCollision(collidedPlatform.GetComponent<Collider>(), player, true);
-            Debug.Log("Hey platorm");
+            Debug.Log(collidedPlatform);      
         }
-        else if (collidedPlatform.transform.position.y <= player.transform.position.y)
+
+        else if (Input.GetKeyDown(KeyCode.DownArrow))
+        {
+            if (collidedPlatform.transform.position.y <= gameObject.transform.position.y)
+            {
+                Debug.Log("Got Here!!!!");
+                Physics.IgnoreCollision(collidedPlatform.GetComponent<Collider>(), player, true);
+
+                
+            }
+        }
+
+        if (collidedPlatform.transform.position.y >= gameObject.transform.position.y)
         {
             Physics.IgnoreCollision(collidedPlatform.GetComponent<Collider>(), player, false);
-            //Debug.Log("player higher now");
         }
 
         if (playerbody.velocity.y <= 0 && gameObject.layer == 12)
         {
             gameObject.layer = 9;
         }
-
     }
 
     private void OnCollisionEnter(Collision collision)
     {
-        collidedPlatform = collision.gameObject;
+        collidedPlatform = collision.gameObject;      
     }
 
     private void OnCollisionExit(Collision collision)
-    {        
-     
+    {
+
     }
 }
