8d712ede5bce44d51f4ff4065716a69d01786d41
TobenTheGreat <33621588+TobenTheGreat@users.noreply.github.com>
2019-03-07 11:49:50 +0100

    Hoppa igenom plattformar

--- Assets/Scripts/JumpScript.cs
+++ Assets/Scripts/JumpScript.cs
@@ -6,29 +6,52 @@ public class JumpScript : MonoBehaviour
 {
     [SerializeField]
     Rigidbody playerbody;
-    Vector3 platformPosition;
+    Collider player;
+
+    [SerializeField]
+    GameObject collidedPlatform;
+
+    //Vector3 platformPosition;
     int colliderValue = 0;
 
     // Start is called before the first frame update
     void Start()
     {
         playerbody = GetComponent<Rigidbody>();
-        Physics.IgnoreLayerCollision(10, 10);
+        player = GetComponent<Collider>();
     }
 
     // Update is called once per frame
     void Update()
     {
+        if (Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            gameObject.layer = 12;
+            Debug.Log(collidedPlatform);
+            //Physics.IgnoreLayerCollision(9, 10);          
+            Physics.IgnoreCollision(collidedPlatform.GetComponent<Collider>(), player, true);
+            Debug.Log("Hey platorm");
+        }
+        else if (collidedPlatform.transform.position.y <= player.transform.position.y)
+        {
+            Physics.IgnoreCollision(collidedPlatform.GetComponent<Collider>(), player, false);
+            //Debug.Log("player higher now");
+        }
 
+        if (playerbody.velocity.y <= 0 && gameObject.layer == 12)
+        {
+            gameObject.layer = 9;
+        }
 
     }
 
     private void OnCollisionEnter(Collision collision)
     {
-        if (collision.collider.gameObject.layer == LayerMask.NameToLayer("Platform"))
-        {
-            Debug.Log("Hey platorm");
-            Physics.IgnoreLayerCollision(9, 10);
-        }
+        collidedPlatform = collision.gameObject;
+    }
+
+    private void OnCollisionExit(Collision collision)
+    {        
+     
     }
 }
