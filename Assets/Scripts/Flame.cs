8f2ed3bcaf0f35904c1bc90f1c2a2c118c0ebf0b
kusmle <48206456+kusmle@users.noreply.github.com>
2019-03-19 12:05:11 +0100

    script och flamethrower prefab
    skript och prefab för flamethower, ej klara

---
+++ Assets/Scripts/Flame.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Player
+{
+    public class Flame : MonoBehaviour
+    {
+        float distTraveled;
+        Vector3 lastPos;
+        Vector3 currentPos;
+        Rigidbody rb;
+        public GameObject Parent { get; set; }
+        bool damageCooldown = false;
+        float fireDamageTime;
+
+        void Start()
+        {
+            lastPos = transform.position;
+            rb = GetComponent<Rigidbody>();
+            rb.velocity = transform.forward * Parent.GetComponent<IWeapon>().ProjectileSpeed;
+            Physics.IgnoreCollision(gameObject.GetComponent<Collider>(), Parent.GetComponent<IWeapon>().Owner.GetComponent<Collider>(), true);
+        }
+
+        private void Update()
+        {
+            distTraveled += Vector3.Distance(transform.position, lastPos);
+            lastPos = transform.position;
+
+            if (distTraveled > 5)
+            {
+                Destroy(gameObject);
+            }
+        }
+
+        //eventuellt flytta till en spelarklass
+        public void DamageImmunity()
+        {
+            if (damageCooldown && fireDamageTime >= 2)
+            {
+                damageCooldown = false;
+            }
+        }
+
+        private void OnTriggerEnter(Collider other)  //code about what happens when the fire hits the opponent
+        {
+            PlayerController playerHit = other.GetComponent<PlayerController>();
+             
+            if (playerHit != null)
+            {
+                if (!damageCooldown)
+                {
+                    playerHit.TakeDamage(Parent.GetComponent<IWeapon>().Damage);
+                    damageCooldown = true;
+                    fireDamageTime = Time.deltaTime;
+                }
+                else
+                {
+                    DamageImmunity();
+                }              
+            }
+
+            if (other.tag != "Weapon")
+            {
+                Destroy(gameObject);
+            }
+        }
+
+
+    }
+}
---
+++ Assets/Scripts/FlameThrower.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+namespace Player
+{
+    public class FlameThrower : MonoBehaviour, IWeapon
+    {
+        public float Damage { get; } = 1f;  //how much damage the weapon does
+        public float ShotsPerSecond { get; } = 30f; // how many shots per second the weapon can shot
+        public float ProjectileSpeed { get; } = 20f;  //the speed of the fire
+
+
+        float currentShotsPerSecond;
+        float shotTimer;
+
+        float timer = 0f;
+        public bool Firing { get; set; } = false;
+
+        [SerializeField]
+        Transform firePoint;
+
+        [SerializeField]
+        GameObject flame;
+
+        public GameObject Owner { get; set; }  //depends on which player picks up the flamethrower
+
+        public void Fire()
+        {
+            GameObject newFlame = Instantiate(flame, firePoint.position, firePoint.rotation);
+            newFlame.GetComponent<Flame>().Parent = gameObject;
+        }    
+
+        
+
+        void Update()
+        {
+            if (Owner == null && Firing)
+            {
+                Firing = false;
+            }
+            if (Firing && Time.time > 1 / ShotsPerSecond + timer)
+            {
+                Fire();
+                timer = Time.time;
+            }
+        }
+
+
+    }
+}
