7ba6d7bbd137e10ba273e08a36954f5128848348
kusmle <48206456+kusmle@users.noreply.github.com>
2019-03-29 14:24:25 +0100

    winner
    winner canvas prefab and script (not done yet)

---
+++ Assets/Scripts/UIScripts/WinnerScript.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class WinnerScript : MonoBehaviour
+{
+    [SerializeField] Canvas WinnerCanvas;
+    [SerializeField] GameObject HumansWin;  //winnerpanel for humans
+    [SerializeField] GameObject BadgersWin;  // winnerpanel for bagders
+    
+    bool humansWon = false;
+    bool badgersWon = false;
+    bool winnerScreen = false;
+
+
+    public void PlayAgain() // code that will reload the current scene (in this case the GameScene)
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+
+    public void MainMenu() // code that goes to MainMenuScene
+    {
+        SceneManager.LoadScene("MainMenuScene");
+
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+       while (winnerScreen)
+        {
+            
+            Time.timeScale = 0f;
+
+            if (humansWon)
+            {
+                HumansWin.SetActive(true);
+            }
+            if (badgersWon)
+            {
+                BadgersWin.SetActive(true);
+            }
+            
+        }
+    }
+}
