56d5a266cae283a6d17fa3bebdb0861c04b6d977
LordCommanderAlokin <niki_96_obrez@hotmail.com>
2019-04-01 10:36:23 +0200

    progressBar

--- Assets/Scripts/ProgressBar.cs
+++ Assets/Scripts/ProgressBar.cs
@@ -4,66 +4,65 @@ using UnityEngine;
 
 namespace Player
 {
-    public class CAM_CamerMovement : MonoBehaviour
+    public class ProgressBar : MonoBehaviour
     {
 
-        public Transform top;
-        public Transform bottom;
-        public Transform target;
+        public Transform dtop;
+        public Transform dbottom;
+        public Transform dtarget;
+
         [SerializeField]
         private float camSpeed = 5f;
-        float yTop;
-        float yBottom;
-        float totalDistance;
-        float pctValue;
+        float dyTop;
+        float dyBottom;
+        float dtotalDistance;
+        float dpctValue;
 
-        [SerializeField] ProgressBar progs;
-
-        public float pctDamage;
+        public float dpctDamage;
 
 
         private void Start()
         {
-            yTop = top.position.y;
-            yBottom = bottom.position.y;
-            totalDistance = yTop - yBottom;
         }
 
         void FixedUpdate()
         {
+            dyTop = dtop.position.y;
+            dyBottom = dbottom.position.y;
+            dtotalDistance = dyTop - dyBottom;
+
             //CalculatePosition();
-            if (target.position != transform.position)
+            if (dtarget.position != transform.position)
             {
                 MoveCamera();
             }
         }
         private void MoveCamera()
         {
-            Vector3 desiredPos = target.position;
+            Vector3 desiredPos = dtarget.position;
             Vector3 smoothPos = Vector3.Lerp(transform.position, desiredPos, camSpeed * Time.deltaTime);
             transform.position = smoothPos;
         }
 
         private void CalculatePosition() //Calcutes the position to which the camera should move to. Win condition needs to be here
         {
-            if (pctDamage > 100)
+            if (dpctDamage > 100)
             {
-                pctDamage = 100;
+                dpctDamage = 100;
                 Debug.Log("Humans win");
             }
-            if (pctDamage < 0)
+            if (dpctDamage < 0)
             {
-                pctDamage = 0;
+                dpctDamage = 0;
                 Debug.Log("Badgers win");
             }
-            pctValue = totalDistance * (pctDamage / 100);
-            Debug.Log(pctValue);
-            target.position = new Vector3(0, yBottom + pctValue, -20);
-            progs.dpctDamage = pctDamage;
+            dpctValue = dtotalDistance * (dpctDamage / 100);
+            Debug.Log(dpctValue);
+            dtarget.position = new Vector3(dbottom.position.x, dyBottom + dpctValue, dbottom.position.z);
         }
         public void ChangeCameraPos(float value)
         {
-            pctDamage += value;
+            dpctDamage += value;
             CalculatePosition();
             //MoveCamera();
         }
