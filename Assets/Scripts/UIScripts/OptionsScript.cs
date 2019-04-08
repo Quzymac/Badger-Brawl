89bda61abdd7aa7b8be27a52db74e8311fae4846
kusmle <48206456+kusmle@users.noreply.github.com>
2019-04-08 14:27:16 +0200

    ljud grupper och oklart options
    alla vapen tillhör ljudgrupper men opionsmenyt e oklart

---
+++ Assets/Scripts/UIScripts/OptionsScript.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+
+public class OptionsScript : MonoBehaviour
+{
+    [SerializeField] GameObject optionpanel;
+    [SerializeField] GameObject screenpanel;
+    [SerializeField] GameObject soundpanel;
+    [SerializeField] AudioMixer am; //our audiomixer from unity
+
+    
+    public void MusicVolume(float music) //this code is used on the gameobject "MusicSlider"
+    {
+        am.SetFloat("BGmusic", music);
+    }
+
+    public void SFXVolume(float sfx) //this code is used on the gameobject "SFXSlider"
+    {
+        am.SetFloat("Sfx", sfx);
+    }
+
+    public void soundChosen()
+    {
+        soundpanel.SetActive(true);
+        optionpanel.SetActive(false);
+    }
+    public void soundBack()
+    {
+        soundpanel.SetActive(false);
+        optionpanel.SetActive(true);
+    }
+
+    public void screenChosen()
+    {
+        screenpanel.SetActive(true);
+        optionpanel.SetActive(false);
+    }
+    public void screenBack()
+    {
+        screenpanel.SetActive(false);
+        optionpanel.SetActive(true);
+    }
+
+    //lånad från Brackeys
+    public void setFullscreen(bool isFullscreen) 
+    {
+        Screen.fullScreen = isFullscreen;
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
