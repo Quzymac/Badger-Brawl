46aad810bfe20eb4f01ead9cbd396f3769c4f374
kusmle <48206456+kusmle@users.noreply.github.com>
2019-04-15 11:00:04 +0200

    uppdaterad creditsmeny och nya partikeleffekter
    uppdaterad creditsmeny och nya parikeleffekter

---
+++ Assets/Scripts/UIScripts/CreditsScript.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CreditsScript : MonoBehaviour
+{
+    [SerializeField] GameObject panelOne;
+    [SerializeField] GameObject panelTwo;
+    [SerializeField] GameObject panelThree;
+    [SerializeField] GameObject panelFour;
+
+
+
+    public void NextOne()
+    {
+        panelTwo.SetActive(true);
+        panelOne.SetActive(false);
+    }
+
+    public void NextTwo()
+    {
+        panelThree.SetActive(true);
+        panelTwo.SetActive(false);
+    }
+
+    public void NextThree()
+    {
+        panelFour.SetActive(true);
+        panelThree.SetActive(false);
+    }
+
+    public void PreviousOne()
+    {
+        panelOne.SetActive(true);
+        panelTwo.SetActive(false);      
+    }
+
+    public void PreviousTwo()
+    {
+        panelTwo.SetActive(true);
+        panelThree.SetActive(false);        
+    }
+    public void PreviousThree()
+    {
+        panelThree.SetActive(true);
+        panelFour.SetActive(false);
+       
+    }
+
+}
