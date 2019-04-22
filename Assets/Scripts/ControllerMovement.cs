6765b9b79586da955d564de0c341cdf6c4c7ee46
TobenTheGreat <33621588+TobenTheGreat@users.noreply.github.com>
2019-04-22 14:14:58 +0100

    Uppdaterade vapen spawning

--- Assets/Scripts/ControllerMovement.cs
+++ Assets/Scripts/ControllerMovement.cs
@@ -1,7 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEditor.Animations;
+//using UnityEditor.Animations;
 
 namespace Player {
     public class ControllerMovement : MonoBehaviour
