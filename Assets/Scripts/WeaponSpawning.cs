e123bf1c747f50f56f37f0684202dd9e6f1f8113
Quzymac <33623813+Quzymac@users.noreply.github.com>
2019-05-09 16:37:03 +0200

    weaponspawning effect + bulletTrail

--- Assets/Scripts/WeaponSpawning.cs
+++ Assets/Scripts/WeaponSpawning.cs
@@ -19,6 +19,11 @@ namespace Player
 
         [SerializeField] float timeBetweenSpawns = 2f;
 
+        Transform lastWeapon;
+        Vector3 startZize = new Vector3(0.15f, 0.15f, 0.15f);
+        Vector3 fullSize = new Vector3(1f, 1f, 1f);
+        [SerializeField] GameObject weaponSpawnEffect;
+        float growthSpeed = 8f;
 
         void Update()
         {
@@ -28,32 +33,44 @@ namespace Player
 
                 if (nextWeaponSpawnTimer < 0)
                 {
-                    SpawnWeapon();
                     nextWeaponSpawnTimer = timeBetweenSpawns;
+                    StartCoroutine(SpawnWeapon());
                 }
+            }
+            if(lastWeapon != null && lastWeapon.localScale.x < 1.0f)
+            {
+                lastWeapon.localScale = Vector3.Lerp(lastWeapon.localScale, fullSize, Time.deltaTime * growthSpeed);
             }
         }
 
-        Transform CheckSpawnPosition() 
+        Vector3 CheckSpawnPosition() 
         {
             while (true)
             {
                 int randomSpawn = Random.Range(0, spawnPositions.Count);
                 if (spawnPositions[randomSpawn].transform.position.y > bottomCollider.position.y + 3 && spawnPositions[randomSpawn].transform.position.y < upperCollider.position.y - 3 && spawnPositions[randomSpawn].GetComponent<WeaponSpawnPosition>().occupied == false)
                 {
-                    return spawnPositions[randomSpawn].transform;
+                    return spawnPositions[randomSpawn].transform.position;
                 }
             }
         }
 
-        public void SpawnWeapon()
+        IEnumerator SpawnWeapon()
         {
+            GameObject effect = Instantiate(weaponSpawnEffect, CheckSpawnPosition(), Quaternion.identity);
+
+            yield return new WaitForSeconds(0.5f);
+
             numberOfWeapons++;
             int randomWeapon = Random.Range(0, weapons.Count);
-            GameObject specificWeapon = Instantiate(weapons[randomWeapon], CheckSpawnPosition().position, Quaternion.Euler(0, 90, 0));
+            GameObject specificWeapon = Instantiate(weapons[randomWeapon], effect.transform.position, Quaternion.Euler(0, 90, 0));
             specificWeapon.GetComponent<WeaponDespawn>().weaponSpawnManager = this;
             spawnedWeapons.Add(specificWeapon);
+
+            lastWeapon = specificWeapon.transform;
+            lastWeapon.localScale = startZize;
         }
+        
         public void DespawnWeapon(GameObject weapon)
         {
             spawnedWeapons.Remove(weapon);
